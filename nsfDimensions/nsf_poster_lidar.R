502afd714239b556be599fc41742e4a13165dad2
ajrominger <ajrominger@gmail.com>
2016-02-27 18:52:43 -0800

    functions to make lidar figure

---
+++ nsfDimensions/nsf_poster_lidar.R
@@ -0,0 +1,6 @@
+## =====================================
+## script to make LiDAR figure for NSF poster
+## =====================================
+
+setwd('~/Dropbox/hawaiiDimensions/presentations/nsfDimensions')
+corepar <- list(fg='black', bg='white', cex.lab=2, cex.axis=1.5)
