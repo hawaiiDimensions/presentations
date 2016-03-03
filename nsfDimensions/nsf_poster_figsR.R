b53a58c06cc798fee43ed6bd44ba40f461ac82ce
ajrominger <ajrominger@gmail.com>
2016-03-03 10:09:40 -0500

    added mete concept fig and metabarcoding experiment results

--- nsfDimensions/nsf_poster_figsR.R
+++ nsfDimensions/nsf_poster_figsR.R
@@ -52,8 +52,9 @@ axis(side=1, at=c(10^(-3:0), 6))
 dev.off()
 
 
-
-##  LiDAR data
+## =================================
+## simulate LiDAR data
+## =================================
 
 library(sp)
 library(raster)
@@ -76,3 +77,70 @@ r <- raster(z, xmn=laupbbox[1, 1], xmx=laupbbox[1, 2], ymn=laupbbox[2, 1], ymx=l
             crs=projection(laup))
 
 writeRaster(r, filename='laup_sim.tif', format='GTiff', overwrite=TRUE)
+
+
+## =================================
+## metabarcoding trial data
+## =================================
+
+x <- read.csv('Spider_DNA_vs_Reads.csv', as.is=TRUE)
+
+pdf(file='fig_metaB_res.pdf', width=5, height=5)
+par(corepar)
+par(mar=c(4, 4, 1, 1) + 0.1, mgp=c(2.75, 1, 0))
+plot(x, xlab='Fraction DNA', ylab='Fraction reads', xlim=range(x), ylim=range(x), cex=2)
+abline(0, 1)
+r2 <- round(summary(lm(x$percentReads ~ x$percentDNA))$r.squared, 2)
+legend('topleft', legend=bquote(R^2==.(r2)), bty='n')
+dev.off()
+
+
+## =================================
+## METE concept fig
+## =================================
+
+Ebreak <- cumsum(sort(0.03 + (1-0.03*12)*rmultinom(1,1000,rexp(11))[,1]/1000,decreasing=TRUE))
+Ebreak <- c(0,Ebreak,1)
+
+indAt <- seq(0.1,0.9,length=12)
+sppAt <- seq(0.3,0.7,by=0.2)
+
+pdf(file='fig_meteExplc.pdf', width=3,height=4)
+par(mar=c(4,0,0,0)+0.1,bg="transparent",fg="black",col.axis="black")
+plot(1,type="n",xlim=c(0.4, 1), ylim=c(0,1),xlab="",axes=FALSE)
+
+rect(0.9,0,1,1,col=hsv(0.42,0.6,0.6),border=NA)
+segments(x0=0.9,x1=1,y0=Ebreak[2:12],col=hsv(0.42,0.6,0.8),lwd=2)
+
+rect(0.65,indAt,0.75,indAt+0.03,col=hsv(0.6,0.3,0.7),border=NA)
+
+for(i in 1:12) {
+    polygon(x=c(0.75,0.75,0.9,0.9),
+            y=c(indAt[i],indAt[i]+0.03,Ebreak[i+1],Ebreak[i]),
+            col=hsv(0.42,0.6,0.8),border=hsv(0.42,0.6,0.8),lwd=0.2)
+    
+    if(i < 3) {
+        polygon(x=c(0.65,0.65,0.45),
+                y=c(indAt[i],indAt[i]+0.03,sppAt[1]),
+                col=hsv(0.6,0.3,0.8),border=hsv(0.6,0.3,0.8))
+    } else if(i < 7) {
+        polygon(x=c(0.65,0.65,0.45),
+                y=c(indAt[i],indAt[i]+0.03,sppAt[2]),
+                col=hsv(0.6,0.3,0.8),border=hsv(0.6,0.3,0.8))
+    } else {
+        polygon(x=c(0.65,0.65,0.45),
+                y=c(indAt[i],indAt[i]+0.03,sppAt[3]),
+                col=hsv(0.6,0.3,0.8),border=hsv(0.6,0.3,0.8))
+    }
+}
+
+points(rep(0.45,3),sppAt,cex=4,pch=21,bg=hsv(0.05,0.7,0.8))
+
+axis(1,at=c(0.45,0.7,0.95),labels=c('Species','Individuals','Energy'))
+
+dev.off()
+
+
+## =================================
+## METE concept fig
+## =================================
